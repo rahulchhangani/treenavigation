1b17d05297070e07c757718e67b6c66d69c664a8
Rahul Chhangani <rahul.chhangani89@gmail.com>
2016-11-15 12:46:56 +0530

    Updated web.xml, Controller Added, Hello World Runs Successfully

--- nutreenavigation/src/main/java/controller/NAVControllerImpl.java
+++ nutreenavigation/src/main/java/controller/NAVControllerImpl.java
@@ -1,6 +1,5 @@
 package controller;
 
-import model.bean.DefaultBean;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/mytree")
 public class NAVControllerImpl implements NAVControllerInterface {
 	
 	//homepage modelview
-		@RequestMapping(value=BASEROOT,method=RequestMethod.GET,produces = "application/json")
+		//@RequestMapping(value=BASEROOT,method=RequestMethod.GET,produces = "application/json")
+	    @RequestMapping("/mytree")
 		public ModelAndView home()
 		{
-			DefaultBean defaultbean = new DefaultBean();
-			return new ModelAndView("index","command",defaultbean);
+			String message = "Hello World";
+			return new ModelAndView("index","message",message);
 		}
 }
