1f19cb8f8dc70c7d37431422c2197e688faaa599
Rahul Chhangani <rahul.chhangani89@gmail.com>
2016-11-16 16:26:10 +0530

    Update dependencies in pom.xml and update controller

--- nutreenavigation/src/main/java/controller/NAVControllerImpl.java
+++ nutreenavigation/src/main/java/controller/NAVControllerImpl.java
@@ -3,7 +3,6 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,7 +13,13 @@ public class NAVControllerImpl implements NAVControllerInterface {
 	    @RequestMapping("/mytree")
 		public ModelAndView home()
 		{
-			String message = "Hello World";
+	    	String message = "Hello World";
 			return new ModelAndView("index","message",message);
 		}
+	    
+	    public String homepageMarker()
+	    {
+	    	
+	    	return null;
+	    }
 }
